e2228749d2fa7a37f1f7a5b90a6f3f5e1d8c52b6
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2020-11-16 22:36:33 -0600

    minor refactor and deleted unnecessary comments and console.logs

--- app.js
+++ app.js
@@ -90,6 +90,7 @@ const engineerQ = [
 ];
 
 //prompter calls itself so that any amount of engineers or interns can be added.
+//I ended up having to add all 3 types of employee so that each array or object carries over into the next iteration without modifying objects in the global scope.
 async function prompter(manager, engineers, interns) {
   inquirer.prompt(promptQ).then((answer) => {
     if (answer.nextQuestion === "Add an Engineer") {
@@ -125,29 +126,8 @@ async function prompter(manager, engineers, interns) {
     }
   });
 }
-// async function engineerBuilder() {
-//   inquirer.prompt(engineerQ).then((answers) => {
-//     return Promise.resolve(
-//       new Engineer(
-//         answers.engineerName,
-//         answers.engineerID,
-//         answers.engineerEmail,
-//         answers.engineerGithub
-//       )
-//     );
-//   });
-// }
-// async function internBuilder() {
-//   inquirer.prompt(internQ).then((answers) => {
-//     new Intern(
-//       answers.internName,
-//       answers.internID,
-//       answers.internEmail,
-//       answers.internSchool
-//     );
-//   });
-// }
 
+//this is where everything actually starts.
 inquirer.prompt(managerQ).then((answers) => {
   const manager = new Manager(
     answers.managerName,
@@ -157,25 +137,3 @@ inquirer.prompt(managerQ).then((answers) => {
   );
   prompter(manager, [], []);
 });
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
